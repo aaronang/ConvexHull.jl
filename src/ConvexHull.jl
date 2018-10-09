9a5bdfebccd6b13c9c5058937eb8c9158069bf69
Aaron Ang <awz.ang@gmail.com>
2018-10-08 23:43:30 -0700

    Implement Graham scan

--- src/ConvexHull.jl
+++ src/ConvexHull.jl
@@ -9,8 +9,30 @@ function Base.isless(p::Point, q::Point)
     p.x < q.x || (p.x == q.x && p.y < q.y)
 end
 
+function isrightturn(p::Point, q::Point, r::Point)
+    (q.x - p.x) * (r.y - p.y) - (q.y - p.y) * (r.x - p.x) < 0
+end
+
 function grahamscan(points::Vector{Point})
-    sort(points)
+    sorted = sort(points)
+    upperhull = sorted[1:2]
+    for p in sorted[3:end]
+        push!(upperhull, p)
+        while length(upperhull) > 2 && !isrightturn(upperhull[end-2:end]...)
+            deleteat!(upperhull, length(upperhull) - 1)
+        end
+    end
+
+    reversed = reverse(sorted)
+    lowerhull = reversed[1:2]
+    for p in reversed[3:end]
+        push!(lowerhull, p)
+        while length(lowerhull) > 2 && !isrightturn(lowerhull[end-2:end]...)
+            deleteat!(lowerhull, length(lowerhull) - 1)
+        end
+    end
+
+    [upperhull..., lowerhull[2:end-1]...]
 end
 
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,4 +1,4 @@
-import ConvexHull: Point, grahamscan
+import ConvexHull: Point, grahamscan, isrightturn
 using Test
 
 @test Point(4.2, 42.0).x == 4.2
@@ -9,3 +9,14 @@ using Test
 @test (Point(1.0, 0.0) < Point(1.0, 1.0)) == true
 @test (Point(1.0, 0.0) < Point(0.0, 1.0)) == false
 @test (Point(1.0, 2.0) < Point(2.0, 1.0)) == true
+
+@test isrightturn(Point(0, 0), Point(0, 1), Point(1, 1)) == true
+@test isrightturn(Point(0, 0), Point(1, 1), Point(2, 1)) == true
+@test isrightturn(Point(0, 0), Point(1, 1), Point(2, 2)) == false
+@test isrightturn(Point(0, 0), Point(1, 1), Point(1, 2)) == false
+@test isrightturn(Point(0, 0), Point(-1, -1), Point(-2, -2)) == false
+@test isrightturn(Point(0, 0), Point(-1, -1), Point(-1, -2)) == false
+@test isrightturn(Point(0, 0), Point(-1, -1), Point(-2, -1)) == true
+
+@test grahamscan([Point(1, 1), Point(-1, -1), Point(-1, 1), Point(1, -1)]) ==
+                 [Point(-1, -1), Point(-1, 1), Point(1, 1), Point(1, -1)]
